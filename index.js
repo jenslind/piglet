8e4b45bc356df76bf4cc0f15a744fd0060fe6912
Jens Lind <jens.lind@oddhill.se>
2015-06-07 23:14:48 +0200

    Added electron gh releases

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = require('app')
 const BrowserWindow = require('browser-window')
 const ipc = require('ipc')
 const fixPath = require('fix-path')
+const Update = require('electron-gh-releases')
 
 // report crashes to the Electron project
 require('crash-reporter').start()
@@ -26,6 +27,26 @@ app.on('ready', function () {
   // Quit app
   ipc.on('app-quit', function () {
     app.quit()
+  })
+
+  // Auto-update
+  let options = {
+    repo: 'jenslind/piglet',
+    currentVersion: app.getVersion()
+  }
+
+  let update = new Update(options, function (auto_updater) {
+    // Auto updater event listener
+    auto_updater.on('update-downloaded', function (e, rNotes, rName, rDate, uUrl, quitAndUpdate) {
+      mainWindow.webContents.send('update-downloaded', quitAndUpdate)
+    })
+  })
+
+  // Check for updates
+  update.check(function (err, status) {
+    if (!err && status) {
+      update.download()
+    }
   })
 
 })
